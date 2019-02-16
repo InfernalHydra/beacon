43511408d68e073f33c1ade634ddd0a986e27b71
Ryan Chhong <ryanchhong@gmail.com>
2019-02-16 14:01:51 -0600

    fixed the map

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import {BrowserRouter, Switch, Route, NavLink, Redirect} from 'react-router-dom'
 import Record from './Record';
 import Map from './Map'
+
 export default class App extends Component
 {
     render()
@@ -10,7 +11,7 @@ export default class App extends Component
             <BrowserRouter>
                 <div id='main-content' style={{height : "100%"}}>
                     <Switch>
-                        <Route path = '/map' Component={() => <Map center={{lat : 32.7767, lng: 96.7970}} zoom={11}/>}/>
+                        <Route path = '/map' component={() => <Map center={{lat : 32.7767, lng: -96.7970}} zoom={11}/>}/>
                         <Route path = '/record' component = {Record}/>
                     </Switch>
                 </div>
