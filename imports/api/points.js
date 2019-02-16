c1a5e795b448ebf946adab6ebcb0a22f79dbbde8
Ryan Chhong <ryanchhong@gmail.com>
2019-02-16 12:02:43 -0600

    Implemented points api

---
+++ imports/api/points.js
@@ -0,0 +1,27 @@
+import { Mongo } from 'meteor/mongo';
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+
+export default Points = new Mongo.Collection('points');
+
+Points.deny({
+    insert() {return true;},
+    update() {return true;},
+    remove() {return true;},
+});
+
+if(Meteor.isServer)
+{
+    Meteor.publish((points) => {
+        return Points.find({});        
+    });
+}
+
+Meteor.methods({
+    'points.add'(latitute, longitude)
+    {
+        check(latitute, Number)
+        check(longitude, Number);
+        Points.add({latitute, longitude, date : new Date()});
+    }
+})
