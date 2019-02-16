3b7eb5645849afd66b6c27e69d50b40ba3fa04d7
Ryan Chhong <ryanchhong@gmail.com>
2019-02-16 13:29:41 -0600

    Began map implementation

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,18 +1,21 @@
 import React, {Component} from 'react'
 import {BrowserRouter, Switch, Route, NavLink, Redirect} from 'react-router-dom'
+import Map from './Map'
 
 export default class App extends Component
 {
     render()
     {
-        <BrowserRouter>
-            <div id='main-content'>
-                <Switch>
-                    <Route exact path = '/' component = {Home}/>
-                    <Route path = '/record' component = {Record}/>
-                    <Route path = '/map' component = {Map} />
-                </Switch>
-            </div>
-        </BrowserRouter>
+        return(
+            <BrowserRouter>
+                <div id='main-content'>
+                    <Switch>
+                        <Route exact path = '/' component = {() => <Map center={{lat : 32.7767, lng: 96.7970}} zoom={11}/>}/>
+                        <Route path = '/record' component = {() => <Map center={{lat : 32.7767, lng: 96.7970}} zoom={11}/>}/>
+                        <Route path = '/map' component = {() => <Map center={{lat : 32.7767, lng: 96.7970}} zoom={11}/>} />
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        );
     }
 }
