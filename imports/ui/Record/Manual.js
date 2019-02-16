94a0afcad58ea6cd8cc19f29942f9f6e0dc1986f
TheBunanna <thebunanna@gmail.com>
2019-02-16 14:03:09 -0600

    Auto stash before merge of "master" and "origin/master"

--- imports/ui/Record/Manual.js
+++ imports/ui/Record/Manual.js
@@ -1,1 +1,10 @@
 import React, {Component} from 'react';
+
+export default class Manual extends Component {
+    constructor (props) {
+        super(props);
+        state = {fields:'None'};        
+    }
+
+    
+}
